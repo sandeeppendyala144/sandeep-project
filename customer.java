ae34527e84c16a93664a01afae9512f9f934e024
sandeeppendyala144 <32168768+sandeeppendyala144@users.noreply.github.com>
2017-09-24 19:53:57 +0530

    Create customer.java

---
+++ customer.java
@@ -0,0 +1,15 @@
+'import java.utill.Scanner;
+class customer
+{
+Scanner sc=new Scanner(System.in)
+String order[]=sc.nextline[];
+String cash[]=sc.nextline[];
+public order(String order,String cash)
+{
+this.order=order;
+this.cash=cash;
+}
+public cash()
+{
+return this.cash;
+}
