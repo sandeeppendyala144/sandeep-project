3f1e01985dcbde2a0ee39ed13ec706cf3c7d4818
sandeeppendyala144 <32168768+sandeeppendyala144@users.noreply.github.com>
2017-09-26 18:17:44 +0530

    Create customerorder.java
    In the above code the customer is giving order to manager..

---
+++ customerorder.java
@@ -0,0 +1,26 @@
+class customerorder extend thread
+{
+customerorder s;
+thread a;
+givenorder(customerorder s)
+{
+this.s=s;
+a=new Thread;
+a.start();
+}
+public void run()
+{
+for(int i=0;i<=2;i++)
+{
+s.customerorder(a);
+}
+}
+class resturant
+{
+public static void main(string[]args)
+{
+customerorder s=new customerorder;
+new.givenorder(a);
+}
+}
+
