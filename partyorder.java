c7d9bc09a51eac17af729bb5b39bcf33d6d63d9e
sandeeppendyala144 <32168768+sandeeppendyala144@users.noreply.github.com>
2017-09-27 18:46:35 +0530

    Create partyorder.java
    The above code takes two inputs one is for type of  party and another is for number of guests. After that depending upon party offer will be given to customers then it will ask you how many types of sweats and how many types of curries..

---
+++ partyorder.java
@@ -0,0 +1,41 @@
+import java.utill.Scanner;
+class partyorder
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in)
+System.out.println("please enter the the party type");
+String s=sc.nextline();
+System.out.println("please enter number of guests");
+int a=sc.nextInt();
+switch(s)
+{
+case"birthday":
+System.out println("for non veg contact office")
+System.out.println("please leave your details for example number of items to be serve///sweats,and curries");
+System.out.println("please enter number of  sweats to be serve ");
+int b=sc.nextInt();
+System.out.println("please enter number of  curries to be serve ");
+int c=sc.nextInt();
+break
+case"marriage":
+System.out println("for non veg contact office")
+System.out.println("please leave your details for example number of items to be serve///sweats,and curries");
+System.out.println("please enter number of  sweats to be serve ");
+int b=sc.nextInt();
+System.out.println("please enter number of  curries to be serve ");
+int c=sc.nextInt();
+break
+case"offical":
+System.out println("for non veg contact office")
+System.out.println("please leave your details for example number of items to be serve///sweats,and curries");
+System.out.println("please enter number of  sweats to be serve ");
+int b=sc.nextInt();
+System.out.println("please enter number of  curries to be serve ");
+int c=sc.nextInt();
+break
+default
+System.out println("error contact office")
+}
+}
+}
