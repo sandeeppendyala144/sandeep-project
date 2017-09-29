1e5c421ab02cae7efa04093c2afb4b355f06a9d5
sandeeppendyala144 <32168768+sandeeppendyala144@users.noreply.github.com>
2017-09-29 10:58:38 +0530

    Create foodtruck.java
    foodtruck is one of the best way of food bussiness where we invest less amount of capital,staff,etc..  and more profit .
    In the above code where customer visit the truck and gives order and enjoy  the food finally gives feedback .
    actually we have one question here why foodtruck? we have restuartant ? answer is simple some times we left with  food due to some reasons so that food will be delivery with the help of this truck.So that their is advertisment and profit to us..

---
+++ foodtruck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class foodtruck
+{
+public static void main(String[]args)
+{
+Scanner scnew Scanner(System.in);
+System.out.println("welcome to the indian restuarant foodtruck");
+System.out.println("please give me your order sir takeway/not");
+String a=sc.next[];
+System.out.println("please give me number of plates");
+int b=sc.nextInt();
+System.out.println("thank you sir could like to give some feedback y/n");
+char c=sc.nextchar();
+switch(c)
+{
+case'y'
+System.out.println("thank you for your feedback sir");
+break;
+case'n'
+System.out.println("thank you sir visit again");
+break;
+}
